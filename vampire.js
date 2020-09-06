8441da5c84b7061468939d2e957d8e775af5db86
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-09-06 13:31:36 -0700

    added new method: countAllDescendants & allMillenialVampires

--- vampire.js
+++ vampire.js
@@ -35,6 +35,58 @@ class Vampire {
 		return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
 	}
 
+	// Returns the vampire object with that name, or null if no vampire exists with that name
+	vampireWithName(name) {
+		let found = null;
+
+		if (this.name === name) {
+			found = this;
+			return found;
+		}
+		for (const os of this.offspring) {
+			console.log('=======');
+			console.log(os);
+			found = os.vampireWithName(name);
+		}
+
+		return found;
+	}
+
+	// Returns the total number of vampires that exist
+	get totalDescendents() {
+		let totalDescendants = 0;
+		const vampire = this;
+
+		const countAllDescendants = (vampire) => {
+			for (const descendant of vampire.offspring) {
+				totalDescendants += 1;
+				countAllDescendants(descendant);
+			}
+		};
+
+		countAllDescendants(vampire);
+		return totalDescendants;
+	}
+
+	// Returns an array of all the vampires that were converted after 1980
+	get allMillennialVampires() {
+		let millenialVampires = [];
+
+		if (this.yearConverted > 1980) {
+			millenialVampires.push(this);
+		}
+
+		// for (const os of this.offspring) {
+		// 	const osMillenial = os.allMillennialVampires;
+		// 	millenialVampires = millenialVampires.concat(osMillenial);
+		// }
+		this.offspring.forEach((os) => {
+			millenialVampires = millenialVampires.concat(os.allMillennialVampires);
+		});
+
+		return millenialVampires;
+	}
+
 	/** Stretch **/
 
 	// Returns the closest common ancestor of two vampires.
@@ -82,25 +134,27 @@ class Vampire {
 // const sarah = new Vampire('Sarah', 2000);
 // const elgort = new Vampire('Elgort', 2001);
 // const hansel = new Vampire('Hansel', 2010);
-// const rootVampire = new Vampire('rv');
-// offspring1 = new Vampire('a');
-// offspring2 = new Vampire('b');
-// offspring3 = new Vampire('c');
-// offspring4 = new Vampire('d');
-// offspring5 = new Vampire('e');
-// offspring6 = new Vampire('f');
-// offspring7 = new Vampire('g');
-// offspring8 = new Vampire('h');
+const rootVampire = new Vampire('rv');
+offspring1 = new Vampire('a', 1980);
+offspring2 = new Vampire('b', 1960);
+offspring3 = new Vampire('c', 1970);
+offspring4 = new Vampire('d', 1981);
+offspring5 = new Vampire('e', 1972);
+offspring6 = new Vampire('f', 1975);
+offspring7 = new Vampire('g', 1985);
+offspring8 = new Vampire('h', 2010);
 
-// rootVampire.addOffspring(offspring1);
-// rootVampire.addOffspring(offspring2);
-// rootVampire.addOffspring(offspring3);
-// offspring3.addOffspring(offspring4);
-// offspring3.addOffspring(offspring5);
-// offspring5.addOffspring(offspring6);
-// offspring6.addOffspring(offspring7);
-// offspring2.addOffspring(offspring8);
+rootVampire.addOffspring(offspring1);
+rootVampire.addOffspring(offspring2);
+rootVampire.addOffspring(offspring3);
+offspring3.addOffspring(offspring4);
+offspring3.addOffspring(offspring5);
+offspring5.addOffspring(offspring6);
+offspring6.addOffspring(offspring7);
+offspring2.addOffspring(offspring8);
 
-// console.log(offspring4.closestCommonAncestor(offspring7));
+// console.log(rootVampire.allMillennialVampires);
+
+console.log(rootVampire.vampireWithName('h'));
 
 module.exports = Vampire;
